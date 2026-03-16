25fd0bfb20b8745d728ff7934023769fbbeced2f
Vadym <geras873@gmail.com>
2026-03-16 23:18:06 +0100

    Add initial project files including configuration, data loaders, and document structure

---
+++ src/config.py
@@ -0,0 +1,18 @@
+class Config:
+    
+    KEYS_FILENAME_TOKEN = "key"
+
+    TABULAR_EXTENSIONS = [
+        ".xlsx",
+        ".xls",
+        ".csv"
+    ]
+
+    KEYS_REQUIRED_COLUMNS = [
+        "issuer_name",
+        "key"
+    ]
+
+    ENTRY_FILE_REQUIRED_COLUMNS = [
+        "item Type"
+    ]
