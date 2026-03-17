534db726f69f93b8c47a6efb224313a6456af904
Vadym <geras873@gmail.com>
2026-03-17 17:56:40 +0100

    Refactor loader to implement dynamic file path retrieval and enhance error handling for input and key value files

--- src/loader.py
+++ src/loader.py
@@ -1,7 +1,50 @@
 import pandas as pd
+from pathlib import Path
 
-entry_path = "data/input/Entry.xlsx"
-key_path = "data/staic/Key values.xlsx"
+def get_files(base_path):
+    
+    input_folder_path = base_path / "data" / "input"
+    key_values_path = base_path / "data" / "static"
+    input_file = []
+    key_value_file = []
 
-entry_df = pd.read_excel(entry_path)
-key_df = pd.read_excel(key_path)
+
+    for f in input_folder_path.iterdir():
+        if f.suffix.lower() in [".xlsx", ".xls", ".csv"] and f.is_file():
+            input_file.append(f)
+
+    if len(input_file) < 1:
+        raise ValueError("Input file is not found")
+    else:
+        if len(input_file) > 1:
+            raise ValueError("More than 1 input file were found")
+
+    for f in key_values_path.iterdir():
+        if (
+            f.suffix.lower() in [".xlsx", ".xls", ".csv"] 
+            and f.is_file() 
+            and "key" in f.name.lower()
+        ):
+            key_value_file.append(f)
+
+    if len(key_value_file) < 1:
+        raise ValueError("Key values file is not found")
+    else:
+        if len(key_value_file) > 1:
+            raise ValueError("More than 1 key values file were found")
+
+    return input_file[0], key_value_file[0]
+
+def read_files(input_file, key_values_file):
+    
+    if input_file.suffix.lower() == ".csv":
+        input_df = pd.read_csv(input_file)
+    else:
+        input_df = pd.read_excel(input_file)
+
+    if key_values_file.suffix.lower() == ".csv":
+        key_values_df = pd.read_csv(key_values_file)
+    else:
+        key_values_df = pd.read_excel(key_values_file)
+
+    return input_df, key_values_df
